30250957f5fe33ce9f11776034c486ab4b5ffa3d
Rebecca Govert <becci.govert@gmail.com>
2018-07-10 15:14:25 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   #emoticons = YAML.load_file('./lib/emoticons.yml')
   YAML.load_file(file_path).each do |emoticon, list|
     engilish, japanese = list
-    
+    library{"get_meaning" =>
 end
 
 def get_japanese_emoticon
